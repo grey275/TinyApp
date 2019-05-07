43f67d74cbd13b284d530d2621b4291be9ff7bde
grey275 <pjvanderpol@gmail.com>
2019-05-07 11:47:34 -0700

    /urls route now displays urls and their keys in a list.

--- express_server.js
+++ express_server.js
@@ -14,7 +14,8 @@ app.get("/", (req, res) => {
 });
 
 app.get('/urls', (req, res) => {
-  res.render(urlDatabase);
+  const templateVars = { urls: urlDatabase };
+  res.render('urls_index', templateVars);
 });
 
 
